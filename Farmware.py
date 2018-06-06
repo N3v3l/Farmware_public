d04f63432d9e134ce3bce0780d57e01f91be8916
N3v3l <39237686+N3v3l@users.noreply.github.com>
2018-06-06 21:54:18 +0200

    Add files via upload

---
+++ Farmware.py
@@ -0,0 +1,278 @@
+import os
+import json
+import requests
+import datetime
+import ast
+import sys
+import math
+import time
+import base64
+
+#timezone
+tz=0
+# long date representation to date object
+def l2d(long_s): return datetime.datetime.strptime(long_s, "%Y-%m-%dT%H:%M:%S.%fZ")
+def s2d(short_s): return datetime.datetime.strptime(short_s, "%Y-%m-%d")
+# date object to long date representation
+def d2l(date): return date.strftime("%Y-%m-%dT%H:%M:%S.%fZ")
+def d2s(date): return date.strftime("%Y-%m-%d")
+def l2u(date): return date-datetime.timedelta(hours=tz)
+def u2l(date): return date+datetime.timedelta(hours=tz)
+def today_utc(): return datetime.datetime.utcnow()
+def today_local(): return today_utc()+datetime.timedelta(hours=tz)
+
+class Weather(object):
+    # ------------------------------------------------------------------------------------------------------------------
+    def __init__(self, fw):
+        self.weather = {}
+        self.fw=fw
+    # ------------------------------------------------------------------------------------------------------------------
+    def __repr__(self):
+        return "Weather()"
+    # ------------------------------------------------------------------------------------------------------------------
+    def __str__(self):
+        l=self.weather.items()
+        l.sort(key=lambda x: s2d(x[0]),reverse=True)
+        ret=""
+        for r in l:
+            ret+="{} : {: >05.2f}mm, [{: >6.2f} -{: >6.2f}]C\n".format(r[0],r[1]['rain24'],r[1]['min_temperature'],r[1]['max_temperature'])
+        return ret
+    # ------------------------------------------------------------------------------------------------------------------
+    def __call__(self):
+        return self.weather
+    # ------------------------------------------------------------------------------------------------------------------
+    def load(self):
+
+        try:
+            weather_station = None
+            try:
+                watering_tool = next(x for x in self.fw.tools() if 'water' in x['name'].lower())
+                weather_station = next(x for x in self.fw.points() if x['pointer_type'] == 'ToolSlot'
+                                       and x['tool_id'] == watering_tool['id'])
+            except Exception as e:
+                self.fw.log("No watering tool detected (I save weather into the watering tool meta)")
+
+            self.weather = ast.literal_eval(weather_station['meta']['current_weather'])
+            if not isinstance(self.weather, dict): raise ValueError
+            # leave only last 7 days
+            self.weather = {k: v for (k, v) in self.weather.items() if
+                            datetime.date.today() - s2d(k).date() < datetime.timedelta(days=7)}
+
+        except:  pass
+
+    # ------------------------------------------------------------------------------------------------------------------
+    def save(self):
+
+        try:
+            watering_tool = next(x for x in self.fw.tools() if 'water' in x['name'].lower())
+            weather_station = next(x for x in self.fw.points() if x['pointer_type'] == 'ToolSlot'
+                                   and x['tool_id'] == watering_tool['id'])
+            weather_station['meta']['current_weather'] = str(self.weather)
+        except:
+            raise ValueError("No watering tool detected (I save weather into the watering tool meta)")
+        self.fw.put('points/{}'.format(weather_station['id']), weather_station)
+
+
+
+class Farmware(object):
+    # ------------------------------------------------------------------------------------------------------------------
+    def __init__(self,app_name):
+        self._points=None
+        self._sequences=None
+        self._tools=None
+        self.args = {}
+        self.debug=False
+        self.local = False
+        self.app_name=app_name
+        self.weather=Weather(self)
+
+        try:
+            self.api_token=os.environ['API_TOKEN']
+            self.headers = {'Authorization': 'Bearer ' + self.api_token, 'content-type': "application/json"}
+            encoded_payload = self.api_token.split('.')[1]
+            encoded_payload += '=' * (4 - len(encoded_payload) % 4)
+            token = json.loads(base64.b64decode(encoded_payload).decode('utf-8'))
+            self.bot_id=token['bot']
+            #self.api_url = 'https:'+token['iss']+'/api/'
+            self.api_url='https://my.farmbot.io/api/'
+            self.mqtt_url=token['mqtt']
+        except :
+            print("API_TOKEN is not set, you gonna have a bad time")
+            sys.exit(1)
+
+    # ------------------------------------------------------------------------------------------------------------------
+    def print_token(self, login, password):
+        data = {'user': {'email': login, 'password': password}}
+        response=self.post('tokens',data)
+
+        print("Device id: {}".format(response['token']['unencoded']['bot']))
+        print("MQTT Host: {}".format(response['token']['unencoded']['mqtt']))
+        print("API_TOKEN: {}".format(response['token']['encoded']))
+        return response
+    # ------------------------------------------------------------------------------------------------------------------
+    def load_config(self):
+        global tz
+        device = self.get('device')
+        tz = device['tz_offset_hrs']
+
+    # ------------------------------------------------------------------------------------------------------------------
+    # loads config parameters
+    def get_arg(self, name, default):
+        try:
+            prefix = self.app_name.lower().replace('-', '_')
+            if type(default)!=tuple:
+                self.args[name] = type(default)(os.environ.get(prefix + '_'+name, default))
+            else:
+                self.args[name] = ast.literal_eval(os.environ.get(prefix + '_' + name, str(default)))
+
+            if self.args[name]=='None': self.args[name]=None
+            if name=='action':
+                if self.args[name]!='real':
+                    if self.args[name] == 'local': self.local = True
+                    self.debug = True
+                    self.log("TEST MODE, NO sequences or movement will be run, plants will NOT be updated",'warn')
+        except:
+            raise ValueError('Error parsing paramenter {}'.format(name))
+
+        return self.args[name]
+
+
+    # ------------------------------------------------------------------------------------------------------------------
+    def log(self, message, message_type='info'):
+
+        try:
+            if not self.local:
+                log_message = '[{}] {}'.format(self.app_name, message)
+                node = {'kind': 'send_message', 'args': {'message': log_message, 'message_type': message_type}}
+                response = requests.post(os.environ['FARMWARE_URL'] + 'api/v1/celery_script', data=json.dumps(node),headers=self.headers)
+                response.raise_for_status()
+                message = log_message
+        except: pass
+
+        print(message)
+
+    # ------------------------------------------------------------------------------------------------------------------
+    def sync(self):
+        if not self.debug:
+            time.sleep(5)
+            node = {'kind': 'sync', 'args': {}}
+            response = requests.post(os.environ['FARMWARE_URL'] + 'api/v1/celery_script', data=json.dumps(node),headers=self.headers)
+            response.raise_for_status()
+
+    # ------------------------------------------------------------------------------------------------------------------
+    def get(self, enpoint):
+        response = requests.get(self.api_url + enpoint, headers=self.headers)
+        response.raise_for_status()
+        return response.json()
+
+    # ------------------------------------------------------------------------------------------------------------------
+    def delete(self, enpoint):
+        if not self.debug:
+            response = requests.delete(self.api_url + enpoint, headers=self.headers)
+            response.raise_for_status()
+            return
+    # ------------------------------------------------------------------------------------------------------------------
+    def post(self, enpoint, data):
+        if not self.debug:
+            response = requests.post(self.api_url + enpoint, headers=self.headers, data=json.dumps(data))
+            response.raise_for_status()
+            return response.json()
+
+    # ------------------------------------------------------------------------------------------------------------------
+    def put(self, enpoint, data):
+        if not self.debug:
+            response = requests.put(self.api_url + enpoint, headers=self.headers, data=json.dumps(data))
+            response.raise_for_status()
+            return response.json()
+
+    # ------------------------------------------------------------------------------------------------------------------
+    def patch(self, enpoint, data):
+        if not self.debug:
+            response = requests.patch(self.api_url + enpoint, headers=self.headers, data=json.dumps(data))
+            response.raise_for_status()
+            return response.json()
+
+    # ------------------------------------------------------------------------------------------------------------------
+    def execute_sequence(self, sequence, message=''):
+        if sequence != None:
+            if message != None:
+                self.log('{}Executing sequence: {}({})'.format(message, sequence['name'].upper(), sequence['id']))
+            if not self.debug:
+                node = {'kind': 'execute', 'args': {'sequence_id': sequence['id']}}
+                response = requests.post(os.environ['FARMWARE_URL'] + 'api/v1/celery_script', data=json.dumps(node),
+                                         headers=self.headers)
+                response.raise_for_status()
+
+    # ------------------------------------------------------------------------------------------------------------------
+    def read_status(self):
+        node = {'kind': 'read_status', 'args': {}}
+        response = requests.post(os.environ['FARMWARE_URL'] + 'api/v1/celery_script', data=json.dumps(node),
+                                 headers=self.headers)
+        response.raise_for_status()
+
+    # ------------------------------------------------------------------------------------------------------------------
+    def move_absolute_safe(self, location, offset={'x': 0, 'y': 0, 'z': 0}, message=''):
+        try:
+            if self.head['z']<location['z']:
+                self.move_absolute({'x':self.head['x'],'y':self.head['y'],'z':location['z']}, {'x': 0, 'y': 0, 'z': 0}, None)
+        except:  pass
+        self.move_absolute(location,offset,message)
+
+    # ------------------------------------------------------------------------------------------------------------------
+    def move_absolute(self, location, offset={'x': 0, 'y': 0, 'z': 0}, message=''):
+
+        if message!=None:
+            self.log('{}Moving absolute: {} {}'.format(message, str(location), "" if offset=={'y': 0, 'x': 0, 'z': 0} else str(offset)))
+
+        node = {'kind': 'move_absolute', 'args':
+            {
+                'location': {'kind': 'coordinate', 'args': location},
+                'offset': {'kind': 'coordinate', 'args': offset},
+                'speed': 300
+            }
+                }
+
+        if not self.debug:
+            response = requests.post(os.environ['FARMWARE_URL'] + 'api/v1/celery_script', data=json.dumps(node),
+                                     headers=self.headers)
+            response.raise_for_status()
+        self.head = {'x': location['x']+offset['x'], 'y': location['y']+offset['y'], 'z': location['y']+offset['y']}
+
+    # ------------------------------------------------------------------------------------------------------------------
+    def points(self):
+        if self._points!=None: return self._points
+        self._points=self.get('points')
+        return self._points
+
+    # ------------------------------------------------------------------------------------------------------------------
+    def plant_age(self, p):
+
+        if p['pointer_type'].lower()!= 'plant': return 0
+        if p['plant_stage'] != 'planted': return 0
+        if p['planted_at'] == None: return 0
+        return (today_utc() - l2d(p['planted_at'])).days + 1
+
+    # ------------------------------------------------------------------------------------------------------------------
+    def sequences(self):
+        if self._sequences != None: return self._sequences
+        self._sequences = self.get('sequences')
+        return self._sequences
+
+    # ------------------------------------------------------------------------------------------------------------------
+    def tools(self):
+        if self._tools != None: return self._tools
+        self._tools = self.get('tools')
+        return self._tools
+
+    # ------------------------------------------------------------------------------------------------------------------
+    def lookup_openfarm(self, plant):
+        response = requests.get(
+            'https://openfarm.cc/api/v1/crops?include=pictures&filter={}'.format(plant['openfarm_slug']), headers=self.headers)
+        response.raise_for_status()
+        return response.json()
+
+    # ------------------------------------------------------------------------------------------------------------------
+    def distance(self, p1, p2):
+        dx=math.fabs(p1['x']-p2['x'])
+        dy = math.fabs(p1['y'] - p2['y'])
+        return math.hypot(dx,dy)
---
+++ main.py
@@ -0,0 +1,146 @@
+import numpy
+from Farmware import *
+
+class Netatmo(Farmware):
+    def __init__(self):
+        Farmware.__init__(self,((__file__.split(os.sep))[len(__file__.split(os.sep)) - 3]).replace('-master', ''))
+
+    # ------------------------------------------------------------------------------------------------------------------
+    def load_config(self):
+
+        super(Netatmo, self).load_config()
+
+        self.private_mode = False
+        try:
+            self.nes=self.get_arg('ne', "(37.80,-122.38)")
+            self.sws = self.get_arg('sw', "(37.70,-122.52)")
+
+            self.ne = ast.literal_eval(self.nes)
+            self.sw = ast.literal_eval(self.sws)
+
+            if not isinstance(self.ne, tuple) or not isinstance(self.ne[0], float) or not isinstance(self.ne[1], float):
+                raise ValueError
+            if not isinstance(self.sw, tuple) or not isinstance(self.sw[0], float) or not isinstance(self.sw[1], float):
+                raise ValueError
+        except:
+            self.private_mode=True
+
+    # ------------------------------------------------------------------------------------------------------------------
+    def get_access_token(self, login='', password=''):
+
+        client_id = '5b183782923dfeefaa8b6adf'
+        client_secret = 'SQa5AwfdreY5sHIJYyNpbORWvSnVV4MO'
+
+        if login != '':
+            payload = {'grant_type': 'password',
+                       'username': login,
+                       'password': password,
+                       'client_id': client_id,
+                       'client_secret': client_secret}
+        else:
+            payload = {'grant_type': 'refresh_token',
+                       'refresh_token': '5b183782923dfeefaa8b6adf|SQa5AwfdreY5sHIJYyNpbORWvSnVV4MO',
+                       'client_id': client_id,
+                       'client_secret': client_secret}
+
+        response = requests.post("https://api.netatmo.com/oauth2/token", data=payload)
+        response.raise_for_status()
+        #print("Your access token is: {}".format(response.json()["access_token"]))
+        #print("Your refresh token is: {}".format(response.json()["refresh_token"]))
+        #print("Expires in: {}m".format(response.json()["expires_in"] / 60))
+
+        return response.json()["access_token"]
+
+
+    # ----------------------------------------------------------------------------------------------------------------------
+    def run(self):
+
+        self.weather.load()
+        td = d2s(today_local())
+        #using private weather station
+        if self.private_mode:
+            self.log('Private mode, contacting your weather station...')
+            params={'access_token': self.get_access_token(self.nes, self.sws)}
+
+            response = requests.post("https://api.netatmo.com/api/getstationsdata", params=params)
+            response.raise_for_status()
+            data = response.json()["body"]
+
+            if len(data['devices']) == 0:
+                raise ValueError("You don't seem to have a weather station?")
+
+            outside = data['devices'][0]['modules'][0]
+            rain = data['devices'][0]['modules'][1]
+
+            self.weather()[td]={}
+            self.weather()[td]['max_temperature']=outside['dashboard_data']['max_temp']
+            self.weather()[td]['min_temperature']=outside['dashboard_data']['min_temp']
+            self.weather()[td]['rain24']=rain['dashboard_data']['sum_rain_24']
+
+        # using public data
+        else:
+            self.log('Community mode: {} - {}'.format(self.ne, self.sw))
+            params = {
+                'access_token': self.get_access_token(),
+                'lat_ne': self.ne[0],
+                'lat_sw': self.sw[0],
+                'lon_ne': self.ne[1],
+                'lon_sw': self.sw[1],
+                'filter': 'true',
+            }
+
+            response = requests.post("https://api.netatmo.com/api/getpublicdata", params=params)
+            response.raise_for_status()
+            data = response.json()["body"]
+
+            if len(data)==0:
+                raise ValueError('No weataher stations found around you, try to enlarge the box')
+
+            self.log("Averaging on {} stations...".format(len(data)))
+            temperature=[]
+            rain24=[]
+
+            for x in data:
+                m=x['measures']
+                for n in m:
+                    if 'res' in m[n]:
+                        for t in m[n]['res']:
+                            for i in range(0,len(m[n]['type'])):
+                                if m[n]['type'][i]=='temperature':
+                                    temperature.append(float(m[n]['res'][t][i]))
+                    if 'rain_24h' in m[n]:
+                        rain24.append(float(m[n]['rain_24h']))
+
+            if len(temperature)==0 or len(rain24)==0:
+                raise ValueError('Did not get data about rain or temperature from stations in this area, try to enlarge the box')
+            mean_t=float('{:.2f}'.format(numpy.mean(temperature)))
+            mean_r=float('{:.2f}'.format(numpy.mean(rain24)))
+
+            if td not in self.weather():
+                self.weather()[td]['min_temperature'] = mean_t
+                self.weather()[td]['max_temperature']=mean_t
+            else:
+                self.weather()[td]['max_temperature'] = max(mean_t,self.weather()[td]['max_temperature'])
+                self.weather()[td]['min_temperature'] = min(mean_t,self.weather()[td]['min_temperature'])
+            self.weather()[td]['rain24'] = mean_r
+
+        self.log('Weather:\n{}'.format(self.weather))
+        self.weather.save()
+
+# ----------------------------------------------------------------------------------------------------------------------
+if __name__ == "__main__":
+
+    app = Netatmo()
+    try:
+        app.load_config()
+        app.run()
+        sys.exit(0)
+
+    except requests.exceptions.HTTPError as error:
+        if error.response.text[0:100]==u'{"error":"invalid_grant"}':
+            app.log('Invalid credentials {} {}'.format(app.nes,app.sws), 'error')
+        else:
+            app.log('HTTP error {} {} '.format(error.response.status_code,error.response.text[0:100]), 'error')
+    except Exception as e:
+        app.log('Something went wrong: {}'.format(str(e)), 'error')
+    sys.exit(1)
